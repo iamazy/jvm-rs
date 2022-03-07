579320dd40d859bb8eb49bcb0d919b106befe3fd
iamazy <1448588084@qq.com>
2022-03-08 00:31:55 +0800

    find class files from dir/jar

--- jvm/src/classpath/entry.rs
+++ jvm/src/classpath/entry.rs
@@ -1,5 +1,7 @@
+use crate::opcode::OpCode::new;
 use std::io::Read;
 use std::path::Path;
+use walkdir::WalkDir;
 
 #[cfg(target_os = "windows")]
 const PATH_SEPARATOR: &str = "\\";
@@ -14,6 +16,9 @@ const PATH_LIST_SEPARATOR: &str = ";";
 const PATH_LIST_SEPARATOR: &str = ":";
 #[cfg(target_os = "macos")]
 const PATH_LIST_SEPARATOR: &str = ":";
+const CLASS_SEPARATOR: &str = ".";
+const CLASS_EXTENSION: &str = "class";
+const JAR_EXTENSION: &str = "jar";
 
 pub trait Entry {
     fn string(&self) -> &String;
@@ -23,14 +28,12 @@ pub trait Entry {
 pub fn new_entry(path: String) -> anyhow::Result<Box<dyn Entry>> {
     if path.contains(PATH_LIST_SEPARATOR) {
         Ok(Box::new(CompositeEntry::new(path)?))
-    } else if path.ends_with(".jar")
-        || path.ends_with(".JAR")
-        || path.ends_with(".zip")
-        || path.ends_with(".ZIP")
-    {
+    } else if path.to_lowercase().ends_with(".jar") {
         Ok(Box::new(ZipEntry::new(path)?))
+    } else if path.ends_with("*") {
+        Ok(Box::new(CompositeEntry::from_wildcard(path)?))
     } else {
-        Err(anyhow::anyhow!("Invalid path: {}", path))
+        Ok(Box::new(DirEntry::new(path)?))
     }
 }
 
@@ -56,9 +59,14 @@ impl Entry for DirEntry {
     }
 
     fn read_class(&self, class_name: &str) -> anyhow::Result<Vec<u8>> {
-        let class_path = format!("{}{}{}", self.path, PATH_SEPARATOR, class_name);
-        let file = std::fs::File::open(class_path.as_str())?;
-        let bytes: Vec<u8> = file.bytes().map(|x| x.unwrap()).collect();
+        let path = format!(
+            "{}{}{}",
+            self.path.trim_end_matches(PATH_SEPARATOR),
+            PATH_SEPARATOR,
+            class_name
+        );
+        let file = std::fs::File::open(path.as_str())?;
+        let bytes: Vec<u8> = file.bytes().map(Result::unwrap).collect();
         Ok(bytes)
     }
 }
@@ -73,12 +81,11 @@ impl ZipEntry {
         let new_path = Path::new(&path);
         if new_path.exists()
             && new_path.is_file()
-            && (new_path.extension() == Some(std::ffi::OsStr::new("zip"))
-                || new_path.extension() == Some(std::ffi::OsStr::new("jar")))
+            && new_path.extension() == Some(std::ffi::OsStr::new(JAR_EXTENSION))
         {
             Ok(Self { path })
         } else {
-            Err(anyhow::anyhow!("{} is not a valid zip file", path))
+            Err(anyhow::anyhow!("{} is not a valid jar file", path))
         }
     }
 }
@@ -111,6 +118,40 @@ impl CompositeEntry {
             .collect::<anyhow::Result<Vec<Box<dyn Entry>>>>()?;
         Ok(Self { entries, path })
     }
+
+    pub fn from_wildcard(path: String) -> anyhow::Result<Self> {
+        let entries = path
+            .split(PATH_LIST_SEPARATOR)
+            .map(|mut path| {
+                let mut entries: Vec<Box<dyn Entry>> = Vec::new();
+                let dirs;
+                if path.ends_with("**") {
+                    path = path.trim_end_matches("**");
+                    dirs = WalkDir::new(path);
+                } else if path.ends_with("*") {
+                    path = path.trim_end_matches("*");
+                    dirs = WalkDir::new(path).max_depth(1);
+                } else {
+                    return entries;
+                }
+                for entry in dirs {
+                    let entry = entry.unwrap();
+                    if entry.file_type().is_file()
+                        && entry.path().extension() != Some(std::ffi::OsStr::new(JAR_EXTENSION))
+                    {
+                        continue;
+                    }
+                    match new_entry(entry.path().to_str().unwrap().to_string()) {
+                        Ok(entry) => entries.push(entry),
+                        Err(_) => (),
+                    }
+                }
+                entries
+            })
+            .flatten()
+            .collect::<Vec<Box<dyn Entry>>>();
+        Ok(Self { entries, path })
+    }
 }
 
 impl Entry for CompositeEntry {
@@ -120,9 +161,15 @@ impl Entry for CompositeEntry {
 
     fn read_class(&self, class_name: &str) -> anyhow::Result<Vec<u8>> {
         for entry in self.entries.iter() {
-            let bytes = entry.read_class(class_name)?;
-            if bytes.len() > 0 {
-                return Ok(bytes);
+            match entry.read_class(class_name) {
+                Ok(bytes) => {
+                    if bytes.len() > 0 {
+                        return Ok(bytes);
+                    } else {
+                        continue;
+                    }
+                }
+                _ => continue,
             }
         }
         Err(anyhow::anyhow!("{} not found", class_name))
@@ -131,8 +178,52 @@ impl Entry for CompositeEntry {
 
 #[cfg(test)]
 mod tests {
+    use crate::classpath::entry::{CompositeEntry, DirEntry, Entry, ZipEntry};
     use classfile::get_utf8;
     use std::io::Read;
+    use walkdir::WalkDir;
+
+    #[test]
+    fn dir_entry() {
+        let entry = DirEntry::new("../data/jvm8/".to_string()).unwrap();
+        let bytes = entry.read_class("HelloWorld").unwrap();
+        assert_eq!(bytes[..4], [0xCA, 0xFE, 0xBA, 0xBE]);
+    }
+
+    #[test]
+    fn zip_entry() {
+        let entry =
+            ZipEntry::new("../data/jvm8/elasticsearch-sql-core-7.16.3.jar".to_string()).unwrap();
+        let bytes = entry
+            .read_class("io/github/iamazy/elasticsearch/dsl/antlr4/ElasticsearchParser.class")
+            .unwrap();
+        assert_eq!(bytes[..4], [0xCA, 0xFE, 0xBA, 0xBE]);
+    }
+
+    #[test]
+    fn composite_entry() {
+        let entry = CompositeEntry::new(
+            "../data/jvm8/elasticsearch-sql-core-7.16.3.jar:../data/jvm8/".to_string(),
+        )
+        .unwrap();
+        let bytes = entry.read_class("HelloWorld.class").unwrap();
+        assert_eq!(bytes[..4], [0xCA, 0xFE, 0xBA, 0xBE]);
+    }
+
+    #[test]
+    fn wildcard_entry() {
+        let entry = CompositeEntry::from_wildcard("../data/jvm8/**".to_string()).unwrap();
+        let bytes = entry.read_class("HelloWorld.class").unwrap();
+        assert_eq!(bytes[..4], [0xCA, 0xFE, 0xBA, 0xBE]);
+
+        let bytes = entry
+            .read_class("io/github/iamazy/elasticsearch/dsl/antlr4/ElasticsearchParser.class")
+            .unwrap();
+        assert_eq!(bytes[..4], [0xCA, 0xFE, 0xBA, 0xBE]);
+        for entry in entry.entries {
+            println!("{}", entry.string());
+        }
+    }
 
     #[test]
     fn read_jar() {
@@ -146,6 +237,16 @@ mod tests {
                 let (_, class_file) = classfile::parse(bytes.as_slice()).unwrap();
                 let raw = get_utf8(class_file.constant_pool, class_file.this_class as usize);
                 println!("{}", String::from_utf8_lossy(raw));
+            }
+        }
+    }
+
+    #[test]
+    fn walk_dir() {
+        for entry in WalkDir::new("../data/jvm8/").max_depth(1) {
+            let entry = entry.unwrap();
+            for path in entry.path().iter() {
+                println!("{}", path.to_str().unwrap());
             }
         }
     }
