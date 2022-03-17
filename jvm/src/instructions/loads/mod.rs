0a23f2550cdae9c0102c62f58eb63450e3842a3a
iamazy <1448588084@qq.com>
2022-03-17 23:31:32 +0800

    clean code with macros

--- jvm/src/instructions/loads/mod.rs
+++ jvm/src/instructions/loads/mod.rs
@@ -1,5 +1,98 @@
-pub(crate) mod aload;
-pub(crate) mod dload;
-pub(crate) mod fload;
-pub(crate) mod iload;
-pub(crate) mod lload;
+use crate::instructions::{InstructionExecutor, InstructionReader};
+use crate::rtda::Frame;
+use bytes::Buf;
+use jvm_macros::{Index8, NoOperand};
+use std::io::Cursor;
+
+macro_rules! register_load_fn {
+    ($(($fn_name:ident, $get_fn:ident, $push_fn:ident)),*) => {
+        $(
+            fn $fn_name(frame: &mut Frame, index: usize) {
+                let val = frame.local_vars().$get_fn(index);
+                frame.operand_stack().$push_fn(val);
+            }
+        )*
+    };
+}
+
+macro_rules! register_load {
+    ($(($load:ident, $load_fn:ident, $val:literal)),*) => {
+        $(
+            #[derive(NoOperand)]
+            #[allow(non_camel_case_types)]
+            pub struct $load;
+
+            impl InstructionExecutor for $load {
+                fn execute(&self, frame: &mut Frame) {
+                    $load_fn(frame, $val);
+                }
+            }
+        )*
+    };
+    ($(($load:ident, $load_fn:ident)),*) => {
+        $(
+            #[derive(Index8)]
+            #[allow(non_camel_case_types)]
+            pub struct $load {
+                index: u32,
+            }
+
+            impl $load {
+                #[inline]
+                pub const fn new(index: u32) -> Self {
+                    Self { index }
+                }
+            }
+
+            impl InstructionExecutor for $load {
+                fn execute(&self, frame: &mut Frame) {
+                    $load_fn(frame, self.index as usize);
+                }
+            }
+        )*
+    };
+}
+
+register_load_fn! {
+    (aload, get_ref, push_ref),
+    (dload, get_double, push_double),
+    (fload, get_float, push_float),
+    (iload, get_int, push_int),
+    (lload, get_long, push_long)
+}
+
+register_load! {
+    (ALOAD, aload),
+    (DLOAD, dload),
+    (FLOAD, fload),
+    (ILOAD, iload),
+    (LLOAD, lload)
+}
+
+register_load! {
+    (ALOAD_0, aload, 0),
+    (ALOAD_1, aload, 1),
+    (ALOAD_2, aload, 2),
+    (ALOAD_3, aload, 3),
+
+    (DLOAD_0, dload, 0),
+    (DLOAD_1, dload, 1),
+    (DLOAD_2, dload, 2),
+    (DLOAD_3, dload, 3),
+
+    (FLOAD_0, fload, 0),
+    (FLOAD_1, fload, 1),
+    (FLOAD_2, fload, 2),
+    (FLOAD_3, fload, 3),
+
+    (ILOAD_0, iload, 0),
+    (ILOAD_1, iload, 1),
+    (ILOAD_2, iload, 2),
+    (ILOAD_3, iload, 3),
+
+    (LLOAD_0, lload, 0),
+    (LLOAD_1, lload, 1),
+    (LLOAD_2, lload, 2),
+    (LLOAD_3, lload, 3)
+
+}
--- jvm/src/instructions/stores/mod.rs
+++ jvm/src/instructions/stores/mod.rs
@@ -1,5 +1,102 @@
-pub(crate) mod astore;
-pub(crate) mod dstore;
-pub(crate) mod fstore;
-pub(crate) mod istore;
-pub(crate) mod lstore;
+use crate::instructions::{InstructionExecutor, InstructionReader};
+use crate::rtda::Frame;
+use bytes::Buf;
+use jvm_macros::{Index8, NoOperand};
+use std::io::Cursor;
+
+macro_rules! register_store_fn {
+    ($(($fn_name:ident, $pop_fn:ident, $set_fn:ident)),*) => {
+        $(
+            fn $fn_name(frame: &mut Frame, index: usize) {
+                let val = frame.operand_stack().$pop_fn();
+                frame.local_vars().$set_fn(index, val);
+            }
+        )*
+    };
+}
+
+macro_rules! register_store {
+    ($(($store:ident, $store_fn:ident, $val:literal)),*) => {
+        $(
+            #[derive(NoOperand)]
+            #[allow(non_camel_case_types)]
+            pub struct $store;
+
+            impl InstructionExecutor for $store {
+                fn execute(&self, frame: &mut Frame) {
+                    $store_fn(frame, $val);
+                }
+            }
+        )*
+    };
+    ($(($store:ident, $store_fn:ident)),*) => {
+        $(
+            #[derive(Index8)]
+            #[allow(non_camel_case_types)]
+            pub struct $store {
+                index: u32,
+            }
+
+            impl $store {
+                #[inline]
+                pub const fn new(index: u32) -> Self {
+                    Self { index }
+                }
+            }
+
+            impl InstructionExecutor for $store {
+                fn execute(&self, frame: &mut Frame) {
+                    $store_fn(frame, self.index as usize);
+                }
+            }
+        )*
+    };
+}
+
+register_store_fn! {
+    (astore, pop_ref, set_ref),
+    (dstore, pop_double, set_double),
+    (fstore, pop_float, set_float),
+    (istore, pop_int, set_int),
+    (lstore, pop_long, set_long)
+}
+
+register_store! {
+    (ASTORE, astore),
+    (DSTORE, dstore),
+    (FSTORE, fstore),
+    (ISTORE, istore),
+    (LSTORE, lstore)
+}
+
+register_store! {
+    // astore
+    (ASTORE_0, astore, 0),
+    (ASTORE_1, astore, 1),
+    (ASTORE_2, astore, 2),
+    (ASTORE_3, astore, 3),
+
+    // dstore
+    (DSTORE_0, dstore, 0),
+    (DSTORE_1, dstore, 1),
+    (DSTORE_2, dstore, 2),
+    (DSTORE_3, dstore, 3),
+
+    // fstore
+    (FSTORE_0, fstore, 0),
+    (FSTORE_1, fstore, 1),
+    (FSTORE_2, fstore, 2),
+    (FSTORE_3, fstore, 3),
+
+    // istore
+    (ISTORE_0, istore, 0),
+    (ISTORE_1, istore, 1),
+    (ISTORE_2, istore, 2),
+    (ISTORE_3, istore, 3),
+
+    // lstore
+    (LSTORE_0, lstore, 0),
+    (LSTORE_1, lstore, 1),
+    (LSTORE_2, lstore, 2),
+    (LSTORE_3, lstore, 3)
+}
