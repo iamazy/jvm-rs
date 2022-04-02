2208281c44aeece17c304254b939d43536beb3cf
iamazy <iamazy.me@outlook.com>
2022-04-02 18:30:40 +0800

    add new instruction

--- jvm/src/instructions/refs/new.rs
+++ jvm/src/instructions/refs/new.rs
@@ -1,10 +1,26 @@
-// #[derive(Index8)]
-// pub struct NEW {
-//     index: usize,
-// }
+use crate::instructions::{InstructionExecutor, InstructionReader};
+use crate::rtda::{Constant, Object};
+use jvm_macros::Index8;
+use bytes::Buf;
 
-// impl InstructionExecutor for NEW {
-//     fn execute(&self, frame: &mut Frame) {
-//         let slot = frame
-//     }
-// }
+
+#[derive(Index8)]
+pub struct NEW {
+    index: usize,
+}
+
+impl InstructionExecutor for NEW {
+    fn execute(&self, frame: &mut crate::rtda::Frame) {
+        unsafe {
+            let mut constant_pool = frame.method().borrow().class.as_ref().constant_pool;
+            if let Constant::Class(class_ref) = constant_pool.as_mut().get_mut(self.index) {
+                let class = class_ref.resolved_class().unwrap();
+                if class.as_ref().is_interface() || class.as_ref().is_abstract() {
+                    panic!("java.lang.InstantiationError");
+                }
+                let mut object = Object::new(class);
+                frame.operand_stack_mut().push_ref(&mut object as *mut Object);
+            }
+        }
+    }
+}
